5bf52eb90f32f47e5b29e1f8ddd574b3ff2503f9
Glutamat42 <markus.heck13396@gmx.de>
2022-12-09 19:39:06 +0100

    fix table names in table seeding

--- db/install.php
+++ db/install.php
@@ -17,14 +17,14 @@ function xmldb_local_adler_install()
     foreach ($modules as $module) {
         if (in_array($module->modname, $module_support['supported_simple']) || in_array($module->modname, $module_support['supported_complex'])) {
             try {
-                $scores_item_id = $DB->insert_record('adler_scores_items',
+                $scores_item_id = $DB->insert_record('local_adler_scores_items',
                     array('type' => 'score',
                         'course_modules_id' => $module->id,
                         'score_min' => 0,
                         'score_max' => 100),
                     $returnid = true, $bulk = false);
                 if ($i % 2 == 0) {
-                    $DB->insert_record('adler_scores_grades',
+                    $DB->insert_record('local_adler_scores_grades',
                         array('scores_items_id' => $scores_item_id,
                             'user_id' => 2,
                             'score' => random_int(0, 100)),
