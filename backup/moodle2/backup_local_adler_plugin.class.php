bf5d638f9d51588bb22dcd7c0db9f285c5d6838b
Glutamat42 <markus.heck13396@gmx.de>
2022-12-19 13:13:41 +0100

    backup/restore logic for score_items

--- backup/moodle2/backup_local_adler_plugin.class.php
+++ backup/moodle2/backup_local_adler_plugin.class.php
@@ -1,38 +1,14 @@
 <?php
 
+defined('MOODLE_INTERNAL') || die();
+
 class backup_local_adler_plugin extends backup_local_plugin
 {
-//    protected function define_course_plugin_structure()
-//    {
-//        $plugin = $this->get_plugin_element(null, null, null);
-//
-//        // To know if we are including userinfo
-//        // not applicable on course level
-//
-//        // Define each element separated
-//        $pluginwrapper = new backup_nested_element($this->get_recommended_name());
-//
-//        $score_types = new backup_nested_element("score_types");
-//        $score_type = new backup_nested_element("score_type", array('id'), array('name', 'timecreated', 'timemodified'));
-//
-//        // Build the tree
-//        $plugin->add_child($pluginwrapper);
-//
-//        $pluginwrapper->add_child($score_types);
-//        $score_types->add_child($score_type);
-//
-//        // Define sources
-//        $score_type->set_source_table("local_adler_scores_types", array('course_id' => backup::VAR_COURSEID));
-////        $score_type->set_source_sql('SELECT * FROM {local_adler_scores_types} WHERE course_id = ?', array(backup::VAR_COURSEID));
-//
-//        // Define id annotations
-//
-//        // Define file annotations
-//
-//        // Return the root element (choice), wrapped into standard activity structure
-//        return $plugin;
-//    }
-
+    /**
+     * Defines the structure of the backup file when backing up an instance of the local Adler plugin.
+     *
+     * @return
+     */
     protected function define_module_plugin_structure()
     {
         $plugin = $this->get_plugin_element(null, null, null);
@@ -44,26 +20,13 @@ class backup_local_adler_plugin extends backup_local_plugin
         $pluginwrapper = new backup_nested_element($this->get_recommended_name());
 
         $score_items = new backup_nested_element("score_items");
-        $score_item = new backup_nested_element("score_item", array('id'), array(
+        $score_item = new backup_nested_element("score_item", null, array(
             'type',
-            'course_modules_id',
             'score_min',
             'score_max',
             'timecreated',
             'timemodified',
         ));
-
-        if($userinfo) {
-            $score_grades = new backup_nested_element("score_grades");
-            $score_grade = new backup_nested_element("score_grade", array('id'), array(
-                'score_items_id',
-                'user_id',
-                'score',
-                'usermodified',
-                'timecreated',
-                'timemodified',
-            ));
-        }
 
         // Build the tree
         $plugin->add_child($pluginwrapper);
@@ -71,28 +34,10 @@ class backup_local_adler_plugin extends backup_local_plugin
         $pluginwrapper->add_child($score_items);
         $score_items->add_child($score_item);
 
-        if ($userinfo) {
-            $pluginwrapper->add_child($score_grades);
-            $score_grades->add_child($score_grade);
-        }
-
         // Define sources
         $score_item->set_source_table('local_adler_scores_items', array('course_modules_id'=>backup::VAR_MODID));
 
-        if ($userinfo) {
-            $score_grade->set_source_sql(
-                // All grades for current CM
-                'SELECT asg.* '.
-                'FROM {local_adler_scores_grades} as asg, {local_adler_scores_items} as asi '.
-                'WHERE asg.scores_items_id = asi.id AND asi.course_modules_id = ?',
-                array(backup_helper::is_sqlparam(backup::VAR_MODID)));
-        }
-
         // Define id annotations
-//        $score_item->annotate_ids('score_item', 'scores_types_id');
-        if ($userinfo) {
-            $score_grade->annotate_ids('user', 'user_id');
-        }
 
         // Define file annotations
 
---
+++ backup/moodle2/restore_local_adler_plugin.class.php
@@ -0,0 +1,42 @@
+<?php
+
+defined('MOODLE_INTERNAL') || die();
+
+/**
+ * Restoring logic for the local Adler plugin.
+ */
+class restore_local_adler_plugin extends restore_local_plugin
+{
+    /**
+     * Defines the structure of the backup file when backing up an instance of the local Adler plugin.
+     *
+     * @return
+     */
+    protected function define_module_plugin_structure()
+    {
+        $paths = [
+            new restore_path_element('score_item', $this->get_pathfor('/score_items/score_item'))
+        ];
+        return $paths;
+    }
+
+    /**
+     * Processes a score item record during the restore process.
+     *
+     * @param object $data The data for the score item.
+     */
+    public function process_score_item($data)
+    {
+        global $DB;
+
+        // Cast $data to object if it is an array
+        // This is done because the object can sometimes randomly be an array
+        $data = (object)$data;
+
+        $cmid = $this->task->get_moduleid();
+        $data->course_modules_id = $cmid;
+
+        // Insert the record into the database
+        $DB->insert_record('local_adler_scores_items', $data);
+    }
+}
