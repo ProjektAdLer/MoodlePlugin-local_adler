2c651ab0fa973d77bc2dbcf03110b445f194e560
Glutamat42 <markus.heck@hs-kempten.de>
2023-02-09 14:55:09 +0100

    update own testcases to load static_mock_framework automatically

--- tests/lib/adler_testcase.php
+++ tests/lib/adler_testcase.php
@@ -4,6 +4,7 @@ namespace local_adler\lib;
 
 global $CFG;
 require_once($CFG->dirroot . '/webservice/tests/helpers.php');
+require_once($CFG->dirroot . '/local/adler/tests/lib/static_mock_framework.php');
 
 use advanced_testcase;
 use externallib_advanced_testcase;
