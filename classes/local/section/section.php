b14019783c0f1e299f2a1c23001fcaa9c0b89d3f
Glutamat42 <markus.heck@hs-kempten.de>
2023-03-30 16:23:15 +0200

    add tests for section/section.php and section/db.php, fix get_adler_section return type

--- classes/local/section/section.php
+++ classes/local/section/section.php
@@ -2,41 +2,44 @@
 namespace local_adler\local\section;
 
 
-use dml_exception;
 use local_adler\adler_score_helpers;
 use local_adler\local\exceptions\not_an_adler_section_exception;
-use moodle_exception;
+use local_adler\static_call_trait;
+use stdClass;
 
 class section {
+    use static_call_trait;
+
     protected int $section_id;
-    protected object $section;
+    protected stdClass $section;
 
     /**
      * @throws not_an_adler_section_exception
-     * @throws dml_exception
      */
     public function __construct(int $section_id) {
         $this->section_id = $section_id;
 
-        $this->section = db::get_adler_section($section_id);
-        if ($this->section === false) {
+        $section = $this->callStatic(db::class, 'get_adler_section', $section_id);
+        if ($section === false) {
             throw new not_an_adler_section_exception();
         }
+        $this->section = $section;
     }
 
-    /**
-     * @throws dml_exception
-     * @throws moodle_exception
+    /** Check if user has enough points to complete this section
+     * @param int $user_id moodle user id
+     * @return bool true if user has completed this section, false otherwise
      */
-    public function is_completed(int $user_id) {
+    public function is_completed(int $user_id): bool {
         // get array of module ids for this section
         $module_ids = array_map(function($module) {
             return $module->id;
-        }, db::get_course_modules_by_section_id($this->section_id));
+        }, $this->callStatic(db::class, 'get_course_modules_by_section_id', $this->section_id));
 
         // get sum of achieved scores for this user
-        $achieved_score = array_sum(adler_score_helpers::get_achieved_scores($module_ids, $user_id));
+        $achieved_scores = $this->callStatic(adler_score_helpers::class, 'get_achieved_scores', $module_ids, $user_id);
+        $score_sum = array_sum($achieved_scores);
 
-        return $achieved_score >= $this->section->required_points_to_complete;
+        return $score_sum >= $this->section->required_points_to_complete;
     }
 }
--- tests/local/section/section_test.php
+++ tests/local/section/section_test.php
@@ -3,21 +3,135 @@
 namespace local_adler\local\section;
 
 
+use completion_info;
+use local_adler\adler_score_helpers;
 use local_adler\lib\local_adler_testcase;
+use local_adler\local\exceptions\not_an_adler_section_exception;
+use ReflectionClass;
 
 global $CFG;
 require_once($CFG->dirroot . '/local/adler/tests/lib/adler_testcase.php');
 
-//require_once($CFG->dirroot . '/local/adler/tests/mocks.php');
-
 
 class section_test extends local_adler_testcase {
-    public function setUp(): void {
-        parent::setUp();
-        $this->adler_generator = $this->getDataGenerator()->get_plugin_generator('local_adler');
+    public function provide_test_construct_data() {
+        return [
+            'valid section' => [
+                'section_response' => (object)[
+                    'id' => 1,
+                    'course_id' => 1,
+                    'name' => 'Test Section',
+                    'required_points_to_complete' => 100
+                ],
+                'exception' => null
+            ],
+            'invalid section' => [
+                'section_response' => false,
+                'exception' => not_an_adler_section_exception::class
+            ]
+        ];
+    }
+
+    /**
+     * @dataProvider provide_test_construct_data
+     */
+    public function test_construct($section_response, $exception) {
+        $return_map = [
+            [db::class, 'get_adler_section', 1, $section_response]
+        ];
+
+        $section_mock = $this->getMockBuilder(section::class)
+            ->disableOriginalConstructor()
+            ->onlyMethods(['callStatic'])
+            ->getMock();
+        $section_mock->method('callStatic')
+            ->will($this->returnValueMap($return_map));
+
+        if ($exception) {
+            $this->expectException($exception);
+        }
+
+        $section_mock->__construct(1);
+
+        // get section property
+        if (!$exception) {
+            $reflection = new ReflectionClass($section_mock);
+            $property = $reflection->getProperty('section');
+            $property->setAccessible(true);
+            $this->assertEquals($section_response, $property->getValue($section_mock));
+        }
     }
 
     public function provide_test_is_completed_data() {
+        return [
+            'completed' => [
+                'modules' => [
+                    1 => 50,
+                    2 => 50,
+                    3 => 50
+                ],
+                'expected' => true
+            ],
+            'not completed' => [
+                'modules' => [
+                    1 => 0,
+                    2 => 0
+                ],
+                'expected' => false
+            ],
+            'edge case completed' => [
+                'modules' => [
+                    1 => 100
+                ],
+                'expected' => true
+            ],
+            'edge case not completed' => [
+                'modules' => [
+                    1 => 99.9
+                ],
+                'expected' => false
+            ],
+        ];
+    }
+
+    /**
+     * @dataProvider provide_test_is_completed_data
+     */
+    public function test_is_completed($modules, $expected) {
+        // mock static function calls
+        $return_map = [
+            [db::class, 'get_course_modules_by_section_id', 1,
+                array_map(function ($id) {
+                    return (object)['id' => $id];
+                }, array_keys($modules))
+            ],
+            [adler_score_helpers::class, 'get_achieved_scores', array_keys($modules), 1, array_values($modules)]
+        ];
+
+        $section_mock = $this->getMockBuilder(section::class)
+            ->disableOriginalConstructor()
+            ->onlyMethods(['callStatic'])
+            ->getMock();
+        $section_mock->method('callStatic')
+            ->will($this->returnValueMap($return_map));
+
+
+        // set section and section_id properties
+        $reflection = new ReflectionClass($section_mock);
+        $property = $reflection->getProperty('section_id');
+        $property->setAccessible(true);
+        $property->setValue($section_mock, 1);
+
+        $property = $reflection->getProperty('section');
+        $property->setAccessible(true);
+        $property->setValue($section_mock, (object)['required_points_to_complete' => 100]);
+
+
+        // call function
+        $this->assertEquals($expected, $section_mock->is_completed(1));
+    }
+
+    public function provide_test_is_completed_integration_data() {
         return [
             'completed' => [
                 'cm_score' => 100,
@@ -31,34 +145,31 @@ class section_test extends local_adler_testcase {
     }
 
     /**
-     * @dataProvider provide_test_is_completed_data
+     * @dataProvider provide_test_is_completed_integration_data
      */
-    public function test_is_completed($cm_score, $expected) {
-        global $DB;
+    public function test_is_completed_integration($cm_score, $expected) {
 
+        // create course and module
         $course = $this->getDataGenerator()->create_course(array('enablecompletion' => 1));
         $module = $this->getDataGenerator()->create_module('assign', ['course' => $course->id, 'completion' => 1]);
-
-
         $section_id = get_fast_modinfo($course->id)->get_cm($module->cmid)->section;
-//        $section = $DB->get_record('course_sections', ['id' => $section_id]);
 
-
-        $this->adler_generator->create_adler_course_object($course->id);
-        $adler_section = $this->adler_generator->create_adler_section_object($section_id);
-        $adler_score = $this->adler_generator->create_adler_score_item($module->cmid, ['score_max' => $cm_score]);
-
+        // create adler entries for course, section and module
+        $adler_generator = $this->getDataGenerator()->get_plugin_generator('local_adler');
+        $adler_generator->create_adler_course_object($course->id);
+        $adler_generator->create_adler_section_object($section_id);
+        $adler_generator->create_adler_score_item($module->cmid, ['score_max' => $cm_score]);
 
         // create user
         $user = $this->getDataGenerator()->create_user();
         $this->getDataGenerator()->enrol_user($user->id, $course->id);
 
         // set module completed for user
-        $completion = new \completion_info($course);
-        $fucking_anderes_course_module_format = get_coursemodule_from_id(null, $module->cmid, 0, false, MUST_EXIST);
-        $completion->update_state($fucking_anderes_course_module_format, COMPLETION_COMPLETE, $user->id);
+        $completion = new completion_info($course);
+        $cm_format2 = get_coursemodule_from_id(null, $module->cmid, 0, false, MUST_EXIST);
+        $completion->update_state($cm_format2, COMPLETION_COMPLETE, $user->id);
 
-
+        // test function
         $section = new section($section_id);
         $result = $section->is_completed($user->id);
         $this->assertEquals($expected, $result);
