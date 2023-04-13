7142302401593e626e2c022852320002606966c4
Glutamat42 <markus.heck@hs-kempten.de>
2023-04-13 21:41:04 +0200

    add event decalaration for course_section_deleted

--- db/events.php
+++ db/events.php
@@ -1,7 +1,7 @@
 <?php
 
 defined('MOODLE_INTERNAL') || die();
-//use local_adler\local_adler_observer;
+
 
 $observers = array(
     array(
@@ -15,5 +15,9 @@ $observers = array(
     array(
         'eventname'   => '\core\event\course_content_deleted',
         'callback'    => '\local_adler\observer::course_content_deleted',
+    ),
+    array(
+        'eventname'   => '\core\event\course_section_deleted',
+        'callback'    => '\local_adler\observer::course_section_deleted',
     )
 );
