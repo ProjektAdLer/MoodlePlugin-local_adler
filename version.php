6a8abd6c72a93c06af970301acc516c2b158c70c
Markus <markus.heck@hs-kempten.de>
2024-10-17 16:25:04 +0200

    bump versions file

--- version.php
+++ version.php
@@ -24,8 +24,8 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->version = 2024101000;
 $plugin->requires = 2022112800;  // Moodle version
 $plugin->component = 'local_adler';
-$plugin->release = '4.0.1';
-$plugin->maturity = MATURITY_STABLE;
+$plugin->release = '4.0.1-dev';
+$plugin->maturity = MATURITY_ALPHA;
 $plugin->dependencies = array(
     'local_logging' => ANY_VERSION,
 );
