6d0255e21ae7ce8c83b43fdcb19b1d671ab704b3
Markus <markus.heck@hs-kempten.de>
2024-12-03 18:32:39 +0100

    fix: upgrade logic for old (authoring tool 2.1.7) courses uploaded via upload_course api did not work correctly

--- classes/local/upgrade/upgrade_3_2_0_to_4_0_0_completionlib.php
+++ classes/local/upgrade/upgrade_3_2_0_to_4_0_0_completionlib.php
@@ -134,7 +134,8 @@ class upgrade_3_2_0_to_4_0_0_completionlib {
         $this->moodle_core_repository->update_course_module_record($cm_info->id, [
             'completion' => 2,
             'completionpassgrade' => $view ? 0 : 1,
-            'completionview' => $view ? 1 : 0
+            'completionview' => $view ? 1 : 0,
+            'completiongradeitemnumber' => 0
         ]);
     }
 }
