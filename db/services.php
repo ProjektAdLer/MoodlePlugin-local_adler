c6605dc44b3a7426dd9a2d2ea69c7c5962d0f1f1
Markus <markus.heck@hs-kempten.de>
2025-03-15 01:33:19 +0100

    add adler_admin_service

--- db/services.php
+++ db/services.php
@@ -22,10 +22,43 @@ $services = array(
         'downloadfiles' => 1,    // allow file downloads.
         'uploadfiles' => 0,      // allow file uploads.
         'loginrequired' => true
+    ),
+    'adler_admin_service' => array(
+        'functions' => array(
+            'local_declarativesetup_create_course_cat_and_assign_user_role',
+//            'core_user_create_users',
+//            'core_course_create_categories',
+//            'core_role_assign_roles',
+            'core_user_delete_users',
+            'core_user_get_users',
+            'enrol_self_enrol_user',
+            'enrol_manual_enrol_users',
+            'core_course_get_courses',
+            'core_enrol_get_enrolled_users',
+            'core_course_get_categories',
+            'core_course_delete_categories'
+        ),
+        'requiredcapability' => 'moodle/site:config',  // not exactly the same as is_siteadmin() but as close as we can get "This capability is intended for administrators only. It is not set for any of the default roles."
+        'restrictedusers' => 0,
+        'enabled' => 1,
+        'shortname' => 'adler_admin_service',
+        'downloadfiles' => 0,
+        'uploadfiles' => 0,
+        'loginrequired' => true
     )
 );
 
 $functions = array(
+    'local_adler_site_admin_login' => [
+        'classname' => 'local_adler\external\site_admin_login',
+        'description' => 'Login as site admin and get token',
+        'type' => 'write',
+        'ajax' => false,
+        'services' => array('adler_admin_service'),
+        'capabilities' => '',
+        'loginrequired' => true
+    ],
+
     'local_adler_trigger_event_cm_viewed' => [         //web service function name
         'classname' => 'local_adler\external\trigger_event_cm_viewed',  //class containing the external function OR namespaced class in classes/external/XXXX.php
         'description' => 'Marks the element as viewed by triggering the "viewed" event for the cm.',    //human readable description of the web service function
