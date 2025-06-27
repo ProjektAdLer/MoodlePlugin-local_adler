0dd822247dda2b23a3b69a394f7e07b885ae8895
Markus <markus.heck@hs-kempten.de>
2025-06-27 20:00:27 +0200

    update for release 6.5.0

--- version.php
+++ version.php
@@ -21,11 +21,11 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2025061101;
+$plugin->version = 2025062700;
 $plugin->requires = 2024042200;  // Moodle version
 $plugin->component = 'local_adler';
-$plugin->release = '6.4.0-dev';
-$plugin->maturity = MATURITY_ALPHA;
+$plugin->release = '6.5.0';
+$plugin->maturity = MATURITY_STABLE;
 $plugin->dependencies = array(
     'local_logging' => ANY_VERSION,
     'local_declarativesetup' => '2025031600',
